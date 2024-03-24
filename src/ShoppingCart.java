a1c452d3173301a4899bbe57ccf61f0a5b7ea2e1
Nehir0109 <nehir.cakilci@gmail.com>
2024-03-24 16:02:03 +0300

    add items to the cart

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -3,4 +3,21 @@ public class ShoppingCart {
     private String[] itemNames;
     private double[] itemPrices;
     private int itemCount;
+
+    public ShoppingCart(){
+        this.itemCount=0;
+        this.itemNames= new String[MAX_ITEMS];
+        this.itemPrices= new double[MAX_ITEMS];
+    }
+
+    private void addItemToCart(String itemName, double itemPrice ){
+        if(itemCount<MAX_ITEMS){
+            itemNames[itemCount]= itemName;
+            itemPrices[itemCount]= itemPrice;
+            itemCount++;
+            System.out.println(itemName+ "added to the cart.");
+        }else{
+            System.out.println("Sorry, cart is full.");
+        }
+    }
 }
